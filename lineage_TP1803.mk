686387de3aef623b011a803c4a04bd79af419e8c
TheScarastic <warabhishek@gmail.com>
2019-08-25 13:27:38 +0530

    TP1803: Initial skeleton

---
+++ lineage_TP1803.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from TP1803 device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := nubia
+PRODUCT_DEVICE := TP1803
+PRODUCT_MANUFACTURER := nubia
+PRODUCT_NAME := lineage_TP1803
+PRODUCT_MODEL := TP1803
+
+PRODUCT_GMS_CLIENTID_BASE := android-nubia
+TARGET_VENDOR := nubia
+TARGET_VENDOR_PRODUCT_NAME := TP1803
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="TP1803-user 9 PKQ1.190504.001 eng.nubia.20190618.151633 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := nubia/TP1803/TP1803:9/PKQ1.190504.001/nubia06181515:user/release-keys
