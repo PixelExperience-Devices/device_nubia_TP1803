39efb8700631b778ffe91c1086bcfe4cdd8c9e3e
TheScarastic <warabhishek@gmail.com>
2019-08-25 16:03:20 +0530

    TP1803: Import Initial properties

---
+++ system_prop.mk
@@ -0,0 +1,56 @@
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.btstack.a2dp_offload_cap=sbc-aac-ldac \
+    persist.vendor.btstack.enable.splita2dp=true \
+    vendor.bluetooth.soc=cherokee
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
+
+# CNE and DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.cne.feature=1
+
+# Data Modules
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.data.mode=concurrent \
+    ro.vendor.use_data_netmgrd=true
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=480
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_hwc_vds=1 \
+    debug.sf.hw=0 \
+    debug.sf.latch_unsignaled=1
+
+# IOP
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.iop.enable_uxe=1 \
+    vendor.perf.iop_v3.enable=true
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.mm.enable.qcom_parser=63963135
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    DEVICE_PROVISIONED=1 \
+    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
+    ril.subscription.types=NV,RUIM \
+    ro.telephony.default_network=22,22 \
+    persist.radio.multisim.config=ssss \
+    persist.rmnet.data.enable=true \
+    persist.vendor.radio.5g_mode_pref=1 \
+    telephony.lteOnCdmaDevice=1
+
+# Sensors
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.sensors.enable.mag_filter=true
+
+# Subsystem ramdump
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.ssr.restart_level=ALL_ENABLE
