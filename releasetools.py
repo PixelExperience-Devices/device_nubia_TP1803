ac9d3d67d34a1f4ecd89e7e107c15b7376eb25d4
Kuba Wojciechowski <nullbytepl@gmail.com>
2020-10-27 22:57:26 +0300

    TP1803: update releasetools extension for R
    * use add-radio-file method instead of old PRODUCT_COPY_FILES
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -19,20 +19,11 @@ import re
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
-  OTA_UpdateFirmware(info)
   return
 
 def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
-  OTA_UpdateFirmware(info)
   return
-
-def OTA_UpdateFirmware(info):
-  info.script.AppendExtra('package_extract_file("install/firmware-update/dtbo.img", "/dev/block/bootdevice/by-name/dtbo");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/vendor.img", "/dev/block/bootdevice/by-name/vendor");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/modem.img", "/dev/block/bootdevice/by-name/modem");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/tz.mbn", "/dev/block/bootdevice/by-name/tz");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/km4.mbn", "/dev/block/bootdevice/by-name/keymaster");')
 
 def AddImage(info, basename, dest):
   name = basename
@@ -40,7 +31,18 @@ def AddImage(info, basename, dest):
   common.ZipWriteStr(info.output_zip, name, data)
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
+def AddImageRadio(info, basename, dest):
+  name = basename
+  data = info.input_zip.read("RADIO/" + basename)
+  common.ZipWriteStr(info.output_zip, name, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+
 def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  AddImageRadio(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImageRadio(info, "vendor.img", "/dev/block/bootdevice/by-name/vendor")
+  AddImageRadio(info, "modem.img", "/dev/block/bootdevice/by-name/modem")
+  AddImageRadio(info, "tz.mbn", "/dev/block/bootdevice/by-name/tz")
+  AddImageRadio(info, "km4.mbn", "/dev/block/bootdevice/by-name/keymaster")
   return
